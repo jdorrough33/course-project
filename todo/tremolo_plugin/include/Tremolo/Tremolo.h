d37444fa67afd1caa071a5c4d53ca98a01644a07
jdorrough33 <jdorrough33@gmail.com>
2026-08-10 14:36:59 -0700

    Completed 3.0.7

--- todo/tremolo_plugin/include/Tremolo/Tremolo.h
+++ todo/tremolo_plugin/include/Tremolo/Tremolo.h
@@ -3,16 +3,31 @@
 namespace tremolo {
 class Tremolo {
 public:
+  Tremolo() {
+    lfo.setFrequency(5.0f, true);
+  }
+
   void prepare(double sampleRate, int expectedMaxFramesPerBlock) {
-    juce::ignoreUnused(sampleRate, expectedMaxFramesPerBlock);
+
+    const juce::dsp::ProcessSpec processSpec {
+      .sampleRate = sampleRate,
+      .maximumBlockSize = static_cast<juce::uint32>(expectedMaxFramesPerBlock),
+      .numChannels = 1u
+    };
+
+    lfo.prepare(processSpec); 
   }
 
   void process(juce::AudioBuffer<float>& buffer) noexcept {
     // for each frame
     for (const auto frameIndex : std::views::iota(0, buffer.getNumSamples())) {
-      // TODO: generate the LFO value
 
+      // TODO: generate the LFO value
+      const auto lfoValue = lfo.processSample(0.0f);
+      
       // TODO: calculate the modulation value
+      constexpr auto modulationDepth = 0.4f;
+      const auto modulationValue  = modulationDepth * lfoValue + 1.0f;
 
       // for each channel sample in the frame
       for (const auto channelIndex :
@@ -21,7 +36,7 @@ public:
         const auto inputSample = buffer.getSample(channelIndex, frameIndex);
 
         // TODO: modulate the sample
-        const auto outputSample = inputSample;
+        const auto outputSample = inputSample * modulationValue;
 
         // set the output sample
         buffer.setSample(channelIndex, frameIndex, outputSample);
@@ -29,9 +44,12 @@ public:
     }
   }
 
-  void reset() noexcept {}
+  void reset() noexcept {
+    lfo.reset();
+  }
 
 private:
   // You should put class members and private functions here
+  juce::dsp::Oscillator<float> lfo{[](float phase) { return std::sin(phase); }};
 };
 }  // namespace tremolo
